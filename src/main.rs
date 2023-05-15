3e09ec327c8f81e2267f367521ded13944957292
Aish <aish@aishraj.com>
2023-05-14 17:31:49 -0700

    A simple red sphere

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,22 @@ use std::io::Write;
 use vec::{Color, Point3, Vec3};
 
 fn ray_color(r: &Ray) -> Color {
+    let center = Point3::new(0.0, 0.0, -1.0);
+    if hit_sphere(center, 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     let unit_direction = r.direction().normalized();
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
 }
 
 fn main() {
